18dd963ad7e108cc8dd88eaacbbb0fde44b04d85
man.nguyen <lotte.dev@terralogic.com>
2023-03-07 17:10:42 +0700

    feat: creat push notification module

--- notification-server/src/app.module.ts
+++ notification-server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PubsubModule } from './modules/pubsub/pubsub.module';
+import { PushNotificationModule } from './modules/push-notification/push-notification.module';
 
 @Module({
-  imports: [PubsubModule],
+  imports: [PubsubModule, PushNotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
